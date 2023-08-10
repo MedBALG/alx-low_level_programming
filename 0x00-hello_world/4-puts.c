db64f41f3cba001009be9473c154df5a309697d7
MedBALG <mohamed.balgoute02@gmail.com>
2023-08-10 16:53:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,10 @@
 *Return: 0
 */
 #include <stdio.h>
+/**
+*main - A program that prints a line with puts function
+*Return: 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle ");
