d2cd69483da5a31e15267e3051e3575d6d0d4a94
MedBALG <mohamed.balgoute02@gmail.com>
2023-10-02 13:33:04 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -267,7 +267,7 @@ if (r == -1)
 {
 free(header);
 close_elf(o);
-dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: %s: No such file\n", argv[1]);
 exit(98);
 }
 check_elf(header->e_ident);
